dec2801b256b98dc422d726dfc988793e66dd648
Niko Köser <koeserniko@gmail.com>
2018-09-12 05:34:42 +0200

    Add project primality

---
+++ projects/primality/primality.c
@@ -0,0 +1,25 @@
+/************************************************************
+ * primality                                                *
+ *                                                          *
+ * -> checking if a number is prime by dividing it by the   *
+ *      numbers up to its square root                       *
+ ************************************************************/
+
+/* includes */
+#include <math.h>
+#include <stdio.h>
+
+/* declarations */
+int primality(int number);
+
+/* main / wrapper function */
+int main()
+{
+    printf("PRIMALITY\n\n");
+    return 0;
+}
+
+/* definitions */
+int primality(const int number)
+{
+}
