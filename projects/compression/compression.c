de5c861e771602c533cfe5477102dca4467aac50
Niko Köser <koeserniko@gmail.com>
2018-09-15 01:01:34 +0200

    Add decode function

--- projects/compression/compression.c
+++ projects/compression/compression.c
@@ -18,8 +18,8 @@ struct compressed_string
     int counts[512];
 };
 
-void run_length_encode(const char *uncompressed_string, struct compressed_string *compr_strg);
-void run_length_decode(struct compressed_string *compr_strg, char *uncompressed_string);
+void run_length_encode(const char *uncompr_string, struct compressed_string *compr_strg);
+void run_length_decode(struct compressed_string *compr_strg, char *uncompr_string);
 
 /* main / wrapper function */
 int main()
@@ -42,6 +42,10 @@ int main()
 
     printf("\n");
 
+    char *dec_string = (char *)malloc(sizeof(char) * length);
+    run_length_decode(&compr_strg, dec_string);
+    printf("%s\n", dec_string);
+
     free(some_string);
 
     return 0;
@@ -49,19 +53,19 @@ int main()
 
 /* definitions */
 /* encode string using run-length algorithm */
-void run_length_encode(const char *uncompressed_string, struct compressed_string *compr_strg)
+void run_length_encode(const char *uncompr_string, struct compressed_string *compr_strg)
 {
-    const int length = strlen(uncompressed_string);
+    const int length = strlen(uncompr_string);
     char letter;
     int letter_counter;
     int compr_strg_counter = 0;
 
     for (int i = 0; i < length; ++i)
     {
-        letter = uncompressed_string[i];
+        letter = uncompr_string[i];
         letter_counter = 1;
 
-        while (uncompressed_string[i] == uncompressed_string[i + 1])
+        while (uncompr_string[i] == uncompr_string[i + 1])
         {
             ++letter_counter;
             ++i;
@@ -80,6 +84,23 @@ void run_length_decode(struct compressed_string *compr_strg, char *uncompressed_
 
     for (int i = 0; i < length; ++i)
     {
-        uncompressed_string
+        int n = compr_strg->counts[i];
+        char letter = compr_strg->letters[i];
+        char letters[n + 1];
+
+        for (int j = 0; j < n; ++j)
+        {
+            letters[j] = letter;
+        }
+
+        letters[n] = '\0';
+
+        if (strlen(uncompressed_string) < 1)
+        {
+            strcpy(uncompressed_string, letters);
+            continue;
+        }
+
+        strcat(uncompressed_string, letters);
     }
 }
