7b0196b2fef1b0c2efebb2766d47c300ff8a517d
Niko Köser <koeserniko@gmail.com>
2018-09-16 16:18:08 +0200

    Fix bug

--- projects/compression/compression.c
+++ projects/compression/compression.c
@@ -20,22 +20,26 @@ struct compressed_string
 
 void run_length_encode(const char *uncompr_string, struct compressed_string *compr_strg);
 void run_length_decode(struct compressed_string *compr_strg, char *uncompr_string);
+void print_compressed_string(struct compressed_string *compr_strg);
 
 /* main / wrapper function */
 int main()
 {
     /* string to compress */
-    const int length = 5;
-    char *some_string = (char *)malloc(sizeof(char) * length);
-    strcpy(some_string, "abcd");
+    const int length = 119;
+    char *some_string = (char *)malloc(length);
+    strcpy(some_string, "aaaaaaaaaaaaaabbbbbbbbbbbbbbbcccccccccccccdddddddddddeeeeeefffffggghhiijjjjkklllmmmnnnnoooppqqrrssstttuuuvvwwwxxxyyyzz");
 
     /* compressed string */
     struct compressed_string compr_strg;
     run_length_encode(some_string, &compr_strg);
+    printf("Compressed:\t");
+    print_compressed_string(&compr_strg);
 
     /* uncompress string */
-    char *dec_string = (char *)malloc(sizeof(char) * length);
+    char *dec_string = (char *)malloc(length);
     run_length_decode(&compr_strg, dec_string);
+    printf("Decompressed:\t%s\n", dec_string);
 
     free(some_string);
 
@@ -66,25 +70,44 @@ void run_length_encode(const char *uncompr_string, struct compressed_string *com
         compr_strg->counts[compr_strg_counter] = letter_counter;
         ++compr_strg_counter;
     }
+
+    compr_strg->letters[compr_strg_counter] = '\0';
 }
 
 /* decode string using run-length algorithm */
 void run_length_decode(struct compressed_string *compr_strg, char *uncompressed_string)
 {
     const int length = strlen(compr_strg->letters);
+    int uncompressed_length = 0;
 
     for (int i = 0; i < length; ++i)
     {
-        int n = compr_strg->counts[i];
-        char letters[n + 1];
+        int count = compr_strg->counts[i];
+        uncompressed_length += count;
+
+        char letters[count + 1];
         char letter = compr_strg->letters[i];
 
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < count; ++j)
         {
             letters[j] = letter;
         }
-
-        letters[n] = '\0';
+        letters[count] = '\0';
         strcat(uncompressed_string, letters);
     }
+
+    uncompressed_string[uncompressed_length] = '\0';
+}
+
+/* print the compressed string struct */
+void print_compressed_string(struct compressed_string *compr_strg)
+{
+    const int length = strlen(compr_strg->letters);
+
+    for (int i = 0; i < length; ++i)
+    {
+        printf("%c%d", compr_strg->letters[i], compr_strg->counts[i]);
+    }
+
+    printf("\n");
 }
