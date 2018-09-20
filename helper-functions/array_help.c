d559743dc24427eb00ea954c5a35adaec1351d64
Niko Köser <koeserniko@gmail.com>
2018-09-20 19:23:42 +0200

    Lower start value in sorted array from 1 to 0

--- helper-functions/array_help.c
+++ helper-functions/array_help.c
@@ -1,18 +1,17 @@
 #include "array_help.h"
 
-#include <stdio.h>                                                                                   
-#include <stdlib.h>                                                                                  
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /* print an array */
 void print_array(int *some_array, const int array_length, const int new_line)
 {
     /* loop through every element */
-    for(int i = 0; i < array_length; ++i)
+    for (int i = 0; i < array_length; ++i)
     {
         /* new row after every n-th element */
-        if(i != 0
-                && i % new_line == 0)
+        if (i != 0 && i % new_line == 0)
         {
             printf("\n");
         }
@@ -28,20 +27,19 @@ void make_random_array(int *random_array, const int array_length)
 {
     srand(time(NULL));
 
-    for(int i = 0; i < array_length; i++)
+    for (int i = 0; i < array_length; i++)
     {
         int random = rand() % 10;
         random_array[i] = random;
     }
 }
 
-
 /* make an array of given length filled with sorted values */
 void make_sorted_array(int *sorted_array, const int array_length)
 {
-    for(int i = 0; i < array_length; ++i)
+    for (int i = 0; i < array_length; ++i)
     {
-        sorted_array[i] = (i + 1);
+        sorted_array[i] = (i);
     }
 }
 
