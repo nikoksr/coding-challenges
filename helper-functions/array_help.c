0cc3006e30c866412e8429ac1bafb44340ce37aa
Niko Köser <koeserniko@gmail.com>
2019-01-31 21:33:39 +0100

    Refactor code

--- helper-functions/array_help.c
+++ helper-functions/array_help.c
@@ -1,44 +1,43 @@
 #include "array_help.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /* print an array */
-void print_array(int* some_array, const int array_length, const int new_line) {
+void print_array(int* arr, const unsigned int arr_len, const unsigned int new_line) {
   /* loop through every element */
-  for (int i = 0; i < array_length; ++i) {
+  for (unsigned int i = 0; i < arr_len; ++i) {
     /* new row after every n-th element */
     if (i != 0 && i % new_line == 0) {
       printf("\n");
     }
 
-    printf("%d ", some_array[i]);
+    printf("%d ", arr[i]);
   }
 
   printf("\n");
 }
 
 /* make an array of given length filled with random values */
-void make_random_array(int* random_array, const int array_length) {
+void make_random_array(int* arr, const unsigned int arr_len) {
   srand(time(NULL));
 
-  for (int i = 0; i < array_length; i++) {
+  for (unsigned int i = 0; i < arr_len; i++) {
     int random = rand() % 10;
-    random_array[i] = random;
+    arr[i] = random;
   }
 }
 
 /* make an array of given length filled with sorted values */
-void make_sorted_array(int* sorted_array, const int array_length) {
-  for (int i = 0; i < array_length; ++i) {
-    sorted_array[i] = (i);
+void make_sorted_array(int* arr, const unsigned int arr_len) {
+  for (unsigned int i = 0; i < arr_len; ++i) {
+    arr[i] = (i);
   }
 }
 
 /* swap the values of two integer variables */
 void swap(int* var_one, int* var_two) {
-  int temp = *var_one;
+  const int temp = *var_one;
   *var_one = *var_two;
   *var_two = temp;
 }
--- helper-functions/array_help.h
+++ helper-functions/array_help.h
@@ -1,14 +1,14 @@
 #ifndef HELPER_FUNCS_H
 #define HELPER_FUNCS_H
 
+/* print an array */
+void print_array(int* arr, const unsigned int arr_len, const unsigned int new_line);
+
 /* make an array of given length filled with random values */
-void make_random_array(int* random_array, const int array_length);
+void make_random_array(int* arr, const unsigned int arr_len);
 
 /* make an array of given length filled with sorted values */
-void make_sorted_array(int* sorted_array, const int array_length);
-
-/* print an array */
-void print_array(int* some_array, const int array_length, const int new_line);
+void make_sorted_array(int* arr, const unsigned int arr_len);
 
 /* swap the values of two integer variables */
 void swap(int* var_one, int* var_two);
