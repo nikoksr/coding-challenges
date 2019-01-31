168fee1ccc06dc32554f3e1b75d3bd6b59432367
Niko Köser <koeserniko@gmail.com>
2019-01-31 18:52:20 +0100

    Refactor code

--- projects/fibonacci/fibonacci.c
+++ projects/fibonacci/fibonacci.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /* fibonacci calculation */
-void fibonacci_factor_limiter(int);
-void fibonacci_number_limiter(int);
+void fibonacci_factor_limiter(const unsigned int factor);
+void fibonacci_number_limiter(const unsigned int num);
 
 /* wrapper function */
 int main() {
@@ -20,33 +20,29 @@ int main() {
 
 /* fibonacci calculations */
 /* maximum factor */
-void fibonacci_factor_limiter(const int factor) {
-  int a = 0, b = 1, c = 0, counter = 1;
-
+void fibonacci_factor_limiter(const unsigned int factor) {
+  unsigned int a = 0, b = 1, c = 0, counter = 1;
   printf("%6d : %6d\n", counter, a);
 
   while (counter < factor && (a + b) < INT_MAX) {
     c = a + b;
     b = a;
     a = c;
-    counter = counter + 1;
-
+    counter += 1;
     printf("%6d : %6d\n", counter, c);
   }
 }
 
 /* maximum number */
-void fibonacci_number_limiter(const int number) {
+void fibonacci_number_limiter(const unsigned int num) {
   int a = 0, b = 1, c = 0, counter = 1;
-
   printf("%6d : %6d\n", counter, a);
 
-  while ((a + b) < number && (a + b) < INT_MAX) {
+  while ((a + b) < num && (a + b) < INT_MAX) {
     c = a + b;
     b = a;
     a = c;
-    counter = counter + 1;
-
+    counter += 1;
     printf("%6d : %6d\n", counter, c);
   }
 }
