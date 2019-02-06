c8a46646753a5895451b6378aa87d4472af07f83
Niko Köser <koeserniko@gmail.com>
2019-02-06 21:23:04 +0100

    Change indentation

--- projects/armstrong_numbers/armstrong_numbers.c
+++ projects/armstrong_numbers/armstrong_numbers.c
@@ -19,55 +19,55 @@ void print_result(const unsigned int num, const unsigned int res);
 
 /* Main function */
 int main() {
-  printf("ARMSTRONG NUMBER\n\n");
+    printf("ARMSTRONG NUMBER\n\n");
 
-  const unsigned int num = 371;
-  const unsigned int res = is_armstrong_number(num);
+    const unsigned int num = 371;
+    const unsigned int res = is_armstrong_number(num);
 
-  print_result(num, res);
-  return 0;
+    print_result(num, res);
+    return 0;
 }
 
 /* Definitions */
 unsigned int is_armstrong_number(const unsigned int num) {
-  if (num < 0) {
-    return 2;
-  }
+    if (num < 0) {
+        return 2;
+    }
 
-  unsigned int n = num;
-  unsigned int armstrong = 0;
-  const unsigned int number_of_digits = floor(log10(n)) + 1;
+    unsigned int n = num;
+    unsigned int armstrong = 0;
+    const unsigned int number_of_digits = floor(log10(n)) + 1;
 
-  while (n > 0) {
-    unsigned int digit = n % 10;
-    n /= 10;
-    armstrong += pow(digit, number_of_digits);
-  }
+    while (n > 0) {
+        unsigned int digit = n % 10;
+        n /= 10;
+        armstrong += pow(digit, number_of_digits);
+    }
 
-  if (armstrong == num) {
-    return 1;
-  }
+    if (armstrong == num) {
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
 
 void print_result(const unsigned int num, const unsigned int res) {
-  char res_text[24];
+    char res_text[24];
 
-  switch (res) {
+    switch (res) {
     case 0:
-      strcpy(res_text, "false");
-      break;
+        strcpy(res_text, "false");
+        break;
     case 1:
-      strcpy(res_text, "true");
-      break;
+        strcpy(res_text, "true");
+        break;
     case 2:
-      strcpy(res_text, "Error: Value smaller 0.");
-      break;
+        strcpy(res_text, "Error: Value smaller 0.");
+        break;
     default:
-      strcpy(res_text, "Unknown error.");
-      break;
-  }
+        strcpy(res_text, "Unknown error.");
+        break;
+    }
 
-  printf("%d -> %s\n", num, res_text);
+    printf("%d -> %s\n", num, res_text);
 }
